4ca37c1710f13cfe376a36b22c4ca39bbd76724f
Hamza <hamzaabdulmajeed18@gmail.com>
2023-01-10 02:20:57 +0500

    Admin_Panel_Bookings

--- GUI/AdminBookingGUI.java
+++ GUI/AdminBookingGUI.java
@@ -1,10 +1,12 @@
 package GUI;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.*;
 
 public class AdminBookingGUI extends JFrame
 {
@@ -33,9 +35,26 @@ public class AdminBookingGUI extends JFrame
             public void actionPerformed(ActionEvent actionEvent)
             {
                 int serial=Integer.parseInt(textField1.getText());
-                //HelpingTicketBookingClass.CancelBooking(Integer.parseInt(textField1.getText()));
-                dispose();
-                new AdminBookingGUI().setVisible(true);
+                try {
+                    Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "online_BusReservation", "group");
+                    System.out.print("Connection Sucessful");
+                    // Create a statement
+                    String sql = "DELETE FROM Ticket_Booking WHERE ticket_id = ?";
+                    PreparedStatement pstmt = conn.prepareStatement(sql);
+
+                    pstmt.setInt(1, serial);
+
+                    // Execute the delete statement
+                    int rowDeleted = pstmt.executeUpdate();
+                    if (rowDeleted > 0) {
+                        System.out.println("A row has been deleted successfully!");
+                        dispose();
+                        new AdminBookingGUI().setVisible(true);
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
             }
         });
     }
@@ -44,73 +63,78 @@ public class AdminBookingGUI extends JFrame
     {
         // TODO: place custom component creation code here
         DefaultTableModel modell=new DefaultTableModel();
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "online_BusReservation", "group");
+            System.out.print("Connection Sucessful");
+            // Create a statement
+            Statement stmt = conn.createStatement();
 
-        modell.addColumn("#");
-        modell.addColumn("Name");
-        modell.addColumn("CNIC#");
-        modell.addColumn("Contact#");
-        modell.addColumn("Date");
-        modell.addColumn("Time");
-        modell.addColumn("To");
-        modell.addColumn("From");
+            // Execute a select query
+            String sql = "SELECT * FROM Ticket_Booking";
+            ResultSet rs = stmt.executeQuery(sql);
 
+            // Get the metadata of the result set
+            ResultSetMetaData rsmd = rs.getMetaData();
 
-       /* for (int i = 0; i< HelpingTicketBookingClass.getTicket(); i++)
-        {
-            String string0,string1,string2,string3,string4,string5,string6,string7;
+            // Get the number of columns in the result set
+            int columnCount = rsmd.getColumnCount();
+            // Add the column names to the model
+            for (int i = 1; i <= columnCount; i++) {
+                modell.addColumn(rsmd.getColumnName(i));
+            }
 
-            //Storing Data
-            string0= String.valueOf(i+1);
-            string1=HelpingTicketBookingClass.getName(i);
-            string2=HelpingTicketBookingClass.getDate(i);
-            string3=HelpingTicketBookingClass.getCNIC(i);
-            string4=HelpingTicketBookingClass.getPhone_Number(i);
-            string5=HelpingTicketBookingClass.getTo(i);
-            string6=HelpingTicketBookingClass.getFrom(i);
-            string7=HelpingTicketBookingClass.getTime(i);
+            // Add the rows to the model
+            while (rs.next()) {
+                Object[] row = new Object[columnCount];
+                for (int i = 1; i <= columnCount; i++) {
+                    row[i - 1] = rs.getObject(i);
+                }
+                modell.addRow(row);
+            }
 
-
-            String[] data={string0,string1,string3,string4,string2,string7,string5,string6};
-            modell.addRow(data);
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(Pannel, ex);
         }
-*/
+
         table1=new JTable(modell);
         table1.setBackground(Color.white);
 
         DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
-
-        //Center Side
+        //Cent
+        // er Side
         DefaultTableCellRenderer Centerrendere=new DefaultTableCellRenderer();
         Centerrendere.setHorizontalAlignment(JLabel.CENTER);
         //Index
-        table1.getColumnModel().getColumn(0).setMaxWidth(40);
-        table1.getColumnModel().getColumn(0).setMinWidth(40);
+        table1.getColumnModel().getColumn(0).setMaxWidth(90);
+        table1.getColumnModel().getColumn(0).setMinWidth(90);
         table1.getColumnModel().getColumn(0).setCellRenderer(centerRenderer);
         //Name
         table1.getColumnModel().getColumn(1).setMinWidth(90);
         table1.getColumnModel().getColumn(1).setMaxWidth(90);
         //CNIC
-        table1.getColumnModel().getColumn(2).setMinWidth(100);
-        table1.getColumnModel().getColumn(2).setMaxWidth(100);
+        table1.getColumnModel().getColumn(2).setMinWidth(140);
+        table1.getColumnModel().getColumn(2).setMaxWidth(140);
         //Contact
-        table1.getColumnModel().getColumn(3).setMinWidth(110);
-        table1.getColumnModel().getColumn(3).setMaxWidth(110);
+        table1.getColumnModel().getColumn(3).setMinWidth(120);
+        table1.getColumnModel().getColumn(3).setMaxWidth(120);
         table1.getColumnModel().getColumn(3).setCellRenderer(centerRenderer);
         //Date
-        table1.getColumnModel().getColumn(4).setMinWidth(100);
-        table1.getColumnModel().getColumn(4).setMaxWidth(100);
+        table1.getColumnModel().getColumn(4).setMinWidth(120);
+        table1.getColumnModel().getColumn(4).setMaxWidth(120);
         table1.getColumnModel().getColumn(4).setCellRenderer(centerRenderer
         );
         //Time
-        table1.getColumnModel().getColumn(5).setMinWidth(60);
-        table1.getColumnModel().getColumn(5).setMaxWidth(60);
+        table1.getColumnModel().getColumn(5).setMinWidth(70);
+        table1.getColumnModel().getColumn(5).setMaxWidth(70);
+
         //To
-        table1.getColumnModel().getColumn(6).setMinWidth(95);
-        table1.getColumnModel().getColumn(6).setMaxWidth(95);
+        table1.getColumnModel().getColumn(6).setMinWidth(100);
+        table1.getColumnModel().getColumn(6).setMaxWidth(100);
         table1.getColumnModel().getColumn(6).setCellRenderer(Centerrendere);
         //From
-        table1.getColumnModel().getColumn(7).setMinWidth(95);
-        table1.getColumnModel().getColumn(7).setMaxWidth(95);
+        table1.getColumnModel().getColumn(7).setMinWidth(100);
+        table1.getColumnModel().getColumn(7).setMaxWidth(100);
         table1.getColumnModel().getColumn(7).setCellRenderer(Centerrendere);
+
     }
 }
